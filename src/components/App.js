208ab50e65623011480d1c2ac2e220887de03fb1
Abdirizak Mohamed <abdirizakm123@gmail.com>
2023-01-12 12:31:23 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function NavBar() {
   return (
     <nav>
@@ -17,6 +18,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+    <h1>About</h1>
+    </div>
+  );
+}
 
 
 function App() {
@@ -24,7 +32,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
